744b35049c0a18252385e9c475b592783b4e7274
Ofir <ofir.almog2@gmail.com>
2026-01-29 15:33:44 +0200

    start converting to databricks premium account

--- dags/airport_pipeline_dag.py
+++ dags/airport_pipeline_dag.py
@@ -1,27 +1,40 @@
+# dags/airport_pipeline_dag.py
 from airflow import DAG
-from airflow.providers.papermill.operators.papermill import PapermillOperator
+from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator
 from datetime import datetime, timedelta
-
 
 default_args = {
     'owner': 'almog',
     'depends_on_past': False,
     'start_date': datetime(2026, 1, 1),
-    'retries': 1,
+    'retries': 2,
     'retry_delay': timedelta(minutes=5),
 }
 
 with DAG(
-    dag_id='airport_pipeline_dag',
-    description='Airport data pipeline',
-    schedule='@daily',  
+    dag_id='airport_pipeline',
+    description='Airport data pipeline - Bronze layer ingestion',
+    schedule='@daily',
     catchup=False,
+    default_args=default_args,
+    tags=['airport', 'flights', 'bronze'],
 ) as dag:
     
-    run_bronze_notebook = PapermillOperator(
-        task_id="run_bronze_ingestion",
-        input_nb="/opt/airflow/bronze_flights_api.ipynb",
-        output_nb="/opt/airflow/logs/out-{{ ds }}.ipynb",
-        parameters={"execution_date": "{{ ds }}"},
-        kernel_name="python3"
+    bronze_ingestion = DatabricksSubmitRunOperator(
+        task_id='bronze_ingestion',
+        databricks_conn_id='databricks_default',
+        notebook_task={
+            'notebook_path': '/Workspace/Users/kseniakor30@gmail.com/airport-data-pipeline/bronze_flights_api',
+            'base_parameters': {
+                'execution_date': '{{ ds }}'
+            }
+        },
+        new_cluster={
+            'spark_version': '13.3.x-scala2.12',
+            'node_type_id': 'i3.xlarge',
+            'num_workers': 2,
+            'spark_conf': {
+                'spark.databricks.delta.preview.enabled': 'true'
+            }
+        }
     )
